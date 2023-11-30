b2561448715f3800e67567d94e1f82c5431c644c
mdtalha178 <talhaazmi178@gmail.com>
2023-12-01 01:47:33 +0530

    version updated

--- exclusive/settings.py
+++ exclusive/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
